22cb807cf777362ea9ac66169ae5cb2be7f34942
WeiserAriel <41187619+WeiserAriel@users.noreply.github.com>
2018-11-26 16:47:22 +0200

    Add files via upload

---
+++ PortScanner/PortScannerUsingNmap.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+'''
+#writing by Ariel Weiser 2018
+'''
+
+import optparse
+import nmap
+import os
+from socket import *
+
+
+def nmapScan(tgtHost, tgtPort):
+	nmScan = nmap.PortScanner()
+	nmScan.scan(tgtHost, tgtPort)
+	state=nmScan[tgtHost]['tcp'][int(tgtPort)]['state']
+	print (" [*] " + tgtHost + " tcp/"+tgtPort+" " +state)
+
+def main():
+	parser = optparse.OptionParser('%prog -H <target host> -p <target port>')
+	parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
+	parser.add_option('-p', dest='tgtPort', type='string', help='specify target ports seperated by commas <eg: 443, 21,22,80>')
+	(options, args) = parser.parse_args()
+	try:
+	#from SOCKET Module
+		tgtHost = gethostbyname(options.tgtHost)
+	except:
+		print ("[-] Cannot resolve '%s': Unknown host" %  tgtHost_
+		exit(0)
+	print ('\n[+] ' + options.tgtHost + ' resolved to ' + str(tgtHost) + '\n')
+	tgtPorts = str(options.tgtPort).split(',')
+
+	if (tgtHost == None) | (tgtPorts[0] == None):
+		print (parser.error('Invalid Arguments.'))
+		exit(0)
+	for tgtPort in tgtPorts:
+		nmapScan(tgtHost, tgtPort)
+
+if __name__ == '__main__':
+	main()
